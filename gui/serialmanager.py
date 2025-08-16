cc6addf5dc269085b7c5e711fdf209c8fcad7e48
TheCrackerSnacker <145797360+TheCrackerSnacker@users.noreply.github.com>
2025-08-15 18:10:48 -0600

    Improved serial port connection on Linux. Hopefully. V2

--- gui/serialmanager.py
+++ gui/serialmanager.py
@@ -58,6 +58,6 @@ def create_serial_connection():
             arduino_name = port.name
     if arduino_name == "":
         raise Exception("Arduino not connected!")
-    if arduino_name[0:2] == "tty":
+    if arduino_name.startswith("tty"):
         arduino_name = "/dev/" + arduino_name
     return Serial(arduino_name, 115200)
